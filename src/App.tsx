d95562b6cefa9b2b005f7dcd60d251f373e2d29a
itsaeed0 <ashourisaeid78@gmail.com>
2025-09-30 13:11:23 +0330

    initial commit, every things we need are installed

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./index.css";
+import React from 'react'
+
+function PrivateRote({children}: {children: React.ReactNode}){
+  const name = localStorage.getItem('user_name')
+  if (!name) return <Navigate to="/login" replace />
+  return <>{children}</>
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<PrivateRote>
+        <Dashboard />
+      </PrivateRote>} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+}
+
+export default App;
